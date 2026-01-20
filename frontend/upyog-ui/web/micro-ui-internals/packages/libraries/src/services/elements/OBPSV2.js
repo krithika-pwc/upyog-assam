5c44fb7a946ac73b1207f706c3dd60641da74550
dbarman123 <124787552+dbarman123@users.noreply.github.com>
2026-01-20 12:46:10 +0530

    DSC implementaion

--- frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/services/elements/OBPSV2.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/services/elements/OBPSV2.js
@@ -329,7 +329,118 @@ export const OBPSV2Services = {
         params: {},
         auth: true,
       }),
-      
+    dscSearchInputToken: ({ tenantId }) =>
+    Request({
+      url: Urls.obpsv2.dscGetInputToken,
+      useCache: false,
+      method: "POST",
+      // userService: auth === false ? auth : true,
+      // auth: true,
+      // params: {},
+    }),
+    dscSearchListToken: (details) =>
+      Request({
+        url: Urls.obpsv2.dscListToken,
+        data: details,
+        useCache: false,
+        setTimeParam: false,
+        userService: true,
+        method: "POST",
+        params: {},
+        auth: true,
+      }),
+    dscSearchToken: (details) =>
+      Request({
+        url: Urls.obpsv2.dscGetToken,
+        data: details,
+        useCache: false,
+        setTimeParam: false,
+        userService: true,
+        method: "POST",
+        params: {},
+        auth: true,
+      }),
+    dscSearchInputCertificate: ({ tenantId }) =>
+      Request({
+        url: Urls.obpsv2.dscGetInputCertificate,
+        useCache: false,
+        method: "POST",
+        // userService: auth === false ? auth : true,
+        // auth: true,
+        // params: {},
+      }),
+    dscSearchListCertificate: (details) =>
+      Request({
+        url: Urls.obpsv2.dscListCertificate,
+        data: details,
+        useCache: false,
+        setTimeParam: false,
+        userService: true,
+        method: "POST",
+        params: {},
+        auth: true,
+      }),
+    dscSearchCertificate: (details) =>
+      Request({
+        url: Urls.obpsv2.dscGetCertificate,
+        data: details,
+        useCache: false,
+        setTimeParam: false,
+        userService: true,
+        method: "POST",
+        params: {},
+        auth: true,
+      }),
+      dscGetFileMetaData: ({ tenantId, fileStoreId }) =>
+      Request({
+        url: Urls.obpsv2.dscMetaData,
+        method: "GET",
+        useCache: false,
+        headers: {
+        "Content-Type": "application/json",
+        "auth-token": Digit.UserService.getUser() ? Digit.UserService.getUser()?.access_token : null,
+        },
+        auth: true,
+        userService: true,
+        params: {
+          tenantId,
+          fileStoreId,
+        },
+      }),
+      dscGetPdfSignInput: (details) =>
+      Request({
+        url: Urls.obpsv2.dscPdfSignInput,
+        data: details,
+        useCache: false,
+        setTimeParam: false,
+        userService: true,
+        method: "POST",
+        params: {},
+        auth: true,
+      }),
+      dscGetPKCSBulkSign: (details) =>
+      Request({
+        url: Urls.obpsv2.dscPKCSBulkSign,
+        data: details,
+        useCache: false,
+        setTimeParam: false,
+        userService: true,
+        method: "POST",
+        params: {},
+        auth: true,
+      }),
+      dscGetPdfSign: (details) =>
+      Request({
+        url: Urls.obpsv2.dscPdfSign,
+        data: details,
+        useCache: false,
+        setTimeParam: false,
+        userService: true,
+        method: "POST",
+        params: {},
+        auth: true,
+      }),
+
   BPAApplicationDetails: async (tenantId, filters) => {
     const response = await OBPSV2Services.search({
       tenantId,
